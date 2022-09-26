8a867c1e563611289a4b4857e07d4a396beb47d3
Dean <deank-52@live.com>
2022-09-26 13:35:22 -0500

    Addition of database connection

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const path = require('path');  
+const path = require('path');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = 3000;
@@ -12,11 +13,31 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'res/login.html'));
 
 });
-  
-app.listen(PORT, (error) =>{
-    if(!error)
-        console.log("Server is Successfully Running, and App is listening on port "+ PORT)
-    else 
-        console.log("Error occurred, server can't start", error);
-    }
-);
+
+// Start Server & Database Connection
+// Note the dg114:test part is the user pass for database
+// this will need to be hidden perhaps using enviroment variables
+// reminder to change
+mongoose.connect('mongodb+srv://dg114:test@cluster0.iwvkvt3.mongodb.net/?retryWrites=true&w=majority')
+.then(()=>{
+    app.listen(PORT, ()=>{
+        console.log("Database & Server started and listening on port " + PORT);
+    })
+})
+.catch((err)=>{
+    console.log("Could not start/connect to server/database");   
+})
+
+// Define Schema (Only an example one for now)
+const studentSchema = new mongoose.Schema ({name: String, age: Number});
+const Student = mongoose.model('Student', studentSchema);
+
+/** Saved a student to database to test (Did work)
+ * const newStudent = new Student({name: "Dean", age: 23});
+ * newStudent.save();
+ */
+
+// Query for the saved student
+Student.find({name: 'Dean'}, function(err,docs) {
+    console.log("Query return for \"name: Dean\"\n" + docs);
+});
