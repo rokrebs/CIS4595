f2d81d29616d7a8c046b595866da52f2beba48e0
Rocio Krebs <rociokrebs@outlook.com>
2022-10-09 21:33:53 -0500

    fixed security errors in LoginController.js

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -2,18 +2,30 @@ import bodyParser from 'body-parser';
 import { mongoose } from 'mongoose';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import {check, validationResult } from 'express-validator';
+
+var loginValidate =[
+  check('usernaame').isLength({min: 3, max: 5}).withMessage("please enter uwf userid").trim().escape(),
+  check('password').isLength({min: 4}).trim().escape()
+]
 
 // Get request for login page
-const loginView = (req, res) => {
+function loginView(req, res) {
   res.status(200);
   res.sendFile(path.join(dirname(fileURLToPath(import.meta.url)), '../resources/HTML/login.html'));
-};
+}
 
 // Post request for logging in users
 const handleLogin = async (req, res) => {
-  console.log(req.body);
-  res.status(200);
-  res.send(`Username: ${req.body.username} Password: ${req.body.password}`);
+  const errors = validationResult(req);
+  if(!errors.isEmpty()){
+    return res.status(422).json({errros: errors.array()});
+  }else{
+    console.log(req.body);
+    res.status(200);
+    res.send(`Username: ${req.body.username} Password: ${req.body.password}`);
+  }
+ 
 };
 
 export {
--- index.js
+++ index.js
@@ -5,13 +5,18 @@ import { router } from './routers/LoginRouter.js';
 import { db } from './config/db.js';
 import { customLimiter } from './customLimitter.js';
 import helmet from 'helmet';
+import csurf from 'csurf';
+import cookieParser from 'cookie-parser';
+
+const csrfProtect = csrf({cookie: true})
+const formParser = bodyParser.urlencoded({extended: false})
 
 const PORT = 3000;
 
 const app = express();
 app.use(helmet.hidePoweredBy()); // hide x powered by header 
-
 app.use(customLimiter);
+app.use(cookieParser());
 
 app.use(express.static('resources'));
 app.use(express.static('view'));
@@ -19,6 +24,7 @@ app.use(express.static('view'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+//vdrf token needs to be implemented
 app.use('/', router);
 
 app.listen(PORT, () => {
