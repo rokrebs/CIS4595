9e3f7caab88d9d286fbf18dcf4e979ec3bdf3099
LightHardt <70411544+LightHardt@users.noreply.github.com>
2022-10-12 18:45:27 -0500

    Update LoginRouter.js

--- routers/LoginRouter.js
+++ routers/LoginRouter.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const login = require('../controllers/LoginController.js');
+const csrf = require('csurf');
 
 const router = express.Router();
 
+router.use(csrf())
 router.get('/', login.loginView);
 router.get('/invalidCredentials', login.invalidCredentials);
 router.post('/', login.handleLogin);
