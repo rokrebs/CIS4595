d6bbb463357586aaf127826b245ebdf01f5b650e
LightHardt <70411544+LightHardt@users.noreply.github.com>
2022-10-12 18:44:40 -0500

    Update appSetup.js

--- config/appSetup.js
+++ config/appSetup.js
@@ -4,10 +4,12 @@ const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
 const loginRouter = require('../routers/LoginRouter.js');
 const limiter = require('../rateLimiter.js');
+const csrf = require('csurf');
 
 const app = express();
 
 /** ** SETUP HTTP *** */
+app.use(csrf())
 app.use(express.static('resources'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
